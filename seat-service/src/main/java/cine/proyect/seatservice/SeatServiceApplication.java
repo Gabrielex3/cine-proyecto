b8181cbcfb363582f1bcada0f0fe595c579a6ab1
vxoo-17 <vxoo.programacion@gmail.com>
2026-05-05 19:02:18 -0400

    repository/dto
    creacion de repository y creacion del dto

--- seat-service/src/main/java/cine/proyect/seatservice/SeatServiceApplication.java
+++ seat-service/src/main/java/cine/proyect/seatservice/SeatServiceApplication.java
@@ -2,12 +2,12 @@ package cine.proyect.seatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SeatServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SeatServiceApplication.class, args);
     }
-
 }

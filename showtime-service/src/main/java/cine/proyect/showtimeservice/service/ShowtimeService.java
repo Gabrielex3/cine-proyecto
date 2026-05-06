52702fcd4c4a8a3b2886509e8178234b521fa863
vxoo-17 <vxoo.programacion@gmail.com>
2026-05-05 20:51:32 -0400

    creacionde controller

---
+++ showtime-service/src/main/java/cine/proyect/showtimeservice/service/ShowtimeService.java
@@ -0,0 +1,52 @@
+package cine.proyect.showtimeservice.service;
+
+import cine.proyect.showtimeservice.dto.ShowtimeRequestDTO;
+import cine.proyect.showtimeservice.model.Showtime;
+import cine.proyect.showtimeservice.repository.ShowtimeRepository; // <--- AGREGA ESTO
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ShowtimeService {
+    private final ShowtimeRepository showtimeRepository;
+
+    public Showtime crearFuncion(ShowtimeRequestDTO dto){
+        log.info("Creando nueva función para la película ID: {}", dto.getMovieId());
+
+        Showtime showtime = new Showtime();
+        showtime.setMovieId(dto.getMovieId());
+        showtime.setRoomId(dto.getRoomId());
+        showtime.setFechaHora(dto.getFechaHora());
+        showtime.setPrecioTicket(dto.getPrecioTicket());
+
+        return showtimeRepository.save(showtime);
+    }
+    public List<Showtime> listarTodas() {
+        return showtimeRepository.findAll();
+    }
+
+    public void eliminar(Long id) {
+        showtimeRepository.deleteById(id);
+    }
+
+    public Showtime buscarPorId(Long id) {
+        return showtimeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("La función con ID " + id + " no existe."));
+    }
+
+    public Showtime actualizar(Long id, ShowtimeRequestDTO dto){
+        Showtime existente = buscarPorId(id);
+
+        existente.setMovieId(dto.getMovieId());
+        existente.setRoomId(dto.getRoomId());
+        existente.setFechaHora(dto.getFechaHora());
+        existente.setPrecioTicket(dto.getPrecioTicket());
+
+        log.info("Actualizando funcion ID: {}", id);
+        return showtimeRepository.save(existente);
+    }
+}

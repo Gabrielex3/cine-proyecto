9fec7c1ecb6c87078abcacc5ff33a1bcaa1d101f
Gabriel Exe <cornejogabriel28@gmail.com>
2026-05-16 01:05:28 -0400

    Updated cinema y conexion mediante openFeing a booking
    Actualizacion de booking y cinema, se agrego la funcionalidad de que al crear la reserva se solicite el id de cine

---
+++ booking-service/src/main/java/cine/proyect/bookingservice/DTO/cinemaDTO.java
@@ -0,0 +1,12 @@
+package cine.proyect.bookingservice.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class cinemaDTO {
+    private String cine;
+
+    private String direccion;
+
+    private Long comunaId;
+}
